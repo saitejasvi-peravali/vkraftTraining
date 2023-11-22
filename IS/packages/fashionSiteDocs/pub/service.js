aeb1679a0b49ecc1ab390cc4a2cd719729039689
saitejasvi <someone@gmail.com>
2023-11-22 10:50:19 +0530

    added fashion site docs

---
+++ IS/packages/fashionSiteDocs/pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://Peravali:5555/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "22-11-2023 10:43:12 IST";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"fashionSiteDocs","createdDate":"12-08-2023 11:41:20 IST","version":"1.0"};
+}
+});
